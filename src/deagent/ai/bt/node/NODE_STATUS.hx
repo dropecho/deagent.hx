bb485a136242b4b4840d577ebdb2c3f16fbeb6ae
Benjamin Van Treese <vantreeseba@gmail.com>
2016-11-25 23:52:58 -0600

    Fixing enum js issue

--- src/deagent/ai/bt/node/NODE_STATUS.hx
+++ src/deagent/ai/bt/node/NODE_STATUS.hx
@@ -1,5 +1,22 @@
 package deagent.ai.bt.node;
 
+#if js
+@:enum
+@:expose("de.bt.NODE_STATUS")
+class NODE_STATUS_IMPL {
+  static public var SUCCESS = 0;
+  static public var FAILURE = 1;
+  static public var RUNNING = 2;
+}
+
+@:enum
+abstract NODE_STATUS(Int) {
+  var SUCCESS = 0;
+  var FAILURE = 1;
+  var RUNNING = 2;
+}
+
+#else
 @:enum
 @:expose("de.bt.NODE_STATUS")
 abstract NODE_STATUS(Int) {
@@ -8,4 +25,4 @@ abstract NODE_STATUS(Int) {
 	var RUNNING = 2;
 }
 
-
+#end
